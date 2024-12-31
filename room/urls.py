d312c7368feae43fa61593dd79f1815b498e45ce
Phelipe Mascarenhas <ph.masc@gmail.com>
2024-12-31 12:22:10 -0300

    Fix: Adjust endpoints

--- room/urls.py
+++ room/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('rooms/list', views.RoomConfigListView.as_view(), name='rooms_list'),
-    path(r'^(?i)(?P<room_name>[a-zA-Z0-9_-]+)/setup/', views.RoomSetupView.as_view(), name='room_setup'),
-    path(r'^(?i)(?P<room_name>[a-zA-Z0-9_-]+)/start/', views.RoomStartView.as_view(), name='room-start'),
-    path(r'^(?i)(?P<room_name>[a-zA-Z0-9_-]+)/gameover/<str:player_name>', views.GameOverView.as_view(), name='game-over'),
-    path(r'^(?i)(?P<room_name>[a-zA-Z0-9_-]+)/result/', views.ResultView.as_view(), name='game-result'),
+    path('<str:room_name>/setup/', views.RoomSetupView.as_view(), name='room_setup'),
+    path('<str:room_name>/start/', views.RoomStartView.as_view(), name='room-start'),
+    path('<str:room_name>/gameover/<str:player_name>', views.GameOverView.as_view(), name='game-over'),
+    path('<str:room_name>/result/', views.ResultView.as_view(), name='game-result'),
 ]
